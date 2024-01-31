a615309e174c8da12f3363b87210026dbf8bd4cf
Feruz M <fmiboy@gmail.com>
2024-01-31 19:04:03 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const client = new dhive.Client('https://rpc.ecency.com');
+const client = new dhive.Client('https://api.hive.blog');
 
 // Generates Aall Private Keys from username and password
 function getPrivateKeys(username, password, roles = ['owner', 'active', 'posting', 'memo']) {
